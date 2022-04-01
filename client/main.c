5ffb8257bdbc13e1eb5795f1a2da9c015f4019d5
Pavel Malyutin <pmalyt@gmail.com>
2022-04-01 18:08:23 +0300

    Client: Implement file transmission.

--- client/main.c
+++ client/main.c
@@ -2,6 +2,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/sendfile.h>
+
 
 int main(int argc,char *argv[])
 {
@@ -22,10 +28,79 @@ int main(int argc,char *argv[])
     //printf("Main: %s\n",args.filename);
 
 
+    struct sockaddr_in srvaddr;
+    srvaddr.sin_family=AF_INET;
+    
+    int p=atoi(args.port);
+    if (p>=1 && p<=0xffff)
+        srvaddr.sin_port=htons( (unsigned short) p);
+    else
+    {
+        puts("Invalid port number");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (inet_pton(AF_INET,args.ip,&srvaddr.sin_addr)==0)
+    {
+        puts("Invalid IPv4 address");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    int skt;
+    if ((skt=socket(AF_INET,SOCK_STREAM,0))==-1)
+    {
+        perror("Socket error");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    if (connect(skt,(struct sockaddr*)&srvaddr,sizeof(srvaddr))==-1)
+    {
+        perror("Connect error");
+        close(fd);
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
+
     
 
+    unsigned char header[256];
+    header[0]=0;
+    while (*(args.filename+header[0])!='\0')
+        ++header[0];
+
+    strncpy(header+1,args.filename,header[0]);
+
+    if (write(skt,header,1+header[0])==-1)
+    {
+        perror("Write error");
+        close(fd);
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
 
 
 
+
+    ssize_t numWrote;
+    while ((numWrote=sendfile(skt,fd,NULL,0x7ffff000))>0)
+        continue;
+    
+    if (numWrote==-1)
+    {
+        perror("Sendfile error");
+        close(fd);
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
+
+
+    close(fd);
+    close(skt);
+    puts("Sent.");
     return 0;
 }
